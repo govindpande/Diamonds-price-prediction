13c709ec1711d63e24f066315abc46ece75cb706
Govind Pande <117948813+govindpande@users.noreply.github.com>
2023-01-13 14:57:08 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from sklearn import datasets, ensemble
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_squared_error
